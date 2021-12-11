f5ea25f16152f35639bc5c6773a59abb5396ca34
Nathaniel Fitzenrider <nathaniel@fitzenrider.com>
2021-12-11 01:10:30 -0500

    Day 7
    This was easy, but my sleep deprived trolling made it harder
    than it should've been.

---
+++ day7/star1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2021 nfitzen <https://github.com/nfitzen>
+
+from statistics import median
+
+
+def main():
+    with open('input.txt') as f:
+        data = [int(i) for i in f.readline().split(',')]
+
+    med = round(median(data))
+
+    print(sum(abs(i - med) for i in data))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day7/star2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2021 nfitzen <https://github.com/nfitzen>
+
+from statistics import median
+
+
+def sum_range(n: int) -> int:
+    return n*(n+1)//2
+
+
+def main():
+    with open('input.txt') as f:
+        data = sorted(int(i) for i in f.readline().split(','))
+
+    best = 0
+    best_fuel = float('inf')
+    for i in range(max(data)):
+        cur_fuel = sum(sum_range(abs(j - i)) for j in data)
+        if cur_fuel < best_fuel:
+            best = i
+            best_fuel = cur_fuel
+
+    print(best_fuel)
+
+
+if __name__ == "__main__":
+    main()
