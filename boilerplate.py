766a85d49f64a8bd700a9fd91d3687dcf17fe6eb
Nathaniel Fitzenrider <nathaniel@fitzenrider.com>
2021-12-11 01:19:27 -0500

    Update boilerplate

--- boilerplate.py
+++ boilerplate.py
@@ -2,13 +2,13 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2021 nfitzen <https://github.com/nfitzen>
 
-
 import itertools
 import math
 
 
 def main():
-    ...
+    with open('input.txt') as f:
+        ...
 
 
 if __name__ == "__main__":
